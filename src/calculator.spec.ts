b3a9dbd87afe7e6159cbc61f0e54e96978e20b3b
robithritz <robithritz@gmail.com>
2019-10-14 12:51:23 +0700

    calculator unit test done

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -7,5 +7,24 @@ describe('Calculator', function () {
         it('should do number addition', function () {
             expect(add(1, 3)).to.be.eq(4);
         })
+
+    })
+    describe('Substract', function () {
+        it('should do number substraction', function () {
+            expect(substract(4, 2)).to.be.eq(2);
+        })
+
+    })
+    describe('Multiple', function () {
+        it('should do number multiplication', function () {
+            expect(multiple(10, 3)).to.be.eq(30);
+        })
+
+    })
+    describe('Divide', function () {
+        it('should do number divison', function () {
+            expect(divide(12, 4)).to.be.eq(3);
+        })
+
     })
 })
